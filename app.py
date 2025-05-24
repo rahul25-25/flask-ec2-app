698aa826bb618d27483568ab698563ccfdf976d8
rahul25-25 <rahulyadavgangavennolla@gmail.com>
2025-05-23 20:54:27 -0700

    Add CI checks: linting + tests, update workflow

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on Rahul!"
+    return "Hello from Flask Nothing Much!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
