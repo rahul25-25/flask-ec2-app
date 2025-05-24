e45b37f5280a7c4df3e3fb97e04657aa90a27d6d
rahul25-25 <rahulyadavgangavennolla@gmail.com>
2025-05-23 19:49:08 -0700

    Describe what change you made

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on EC2!"
+    return "Hello from Flask on Rahul!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
